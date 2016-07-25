6dac4f589a52bd245dd45e683053a145a9cb98ae
RapidFiring <rapid.firing.eve@gmail.com>
2016-07-25 17:24:57 +0200

    Main component - App - routes

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+/**
+ * Created by MWalters on 25.07.2016.
+ */
+import React, {PropTypes} from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <p>Header here...</p>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
+export default App;
